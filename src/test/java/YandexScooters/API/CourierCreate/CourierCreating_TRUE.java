791264cd6800a0af0b898a49648243d3f752ad01
psucho95 <psucho604@gmail.com>
2023-04-03 17:27:01 +0500

    Added new tests

--- src/test/java/YandexScooters/API/CourierCreate/CourierCreating_TRUE.java
+++ src/test/java/YandexScooters/API/CourierCreate/CourierCreating_TRUE.java
@@ -3,18 +3,15 @@ package YandexScooters.API.CourierCreate;
 import Scooter_Data.API.BasePage_API;
 import Scooter_Data.API.JSONs.Generator_JSON_createData;
 import Scooter_Data.API.RequestDeserializer.Deserializer_courierCreate;
-import io.qameta.allure.Description;
 import io.qameta.allure.junit4.DisplayName;
 import io.restassured.response.Response;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.JUnit4;
 
 import static Scooter_Data.API.CommonData.StaticData.*;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.*;
 
-public class CourierCreating_TRUE_Test extends BasePage_API {
+public class CourierCreating_TRUE extends BasePage_API {
 	@Test
 	@DisplayName("Курьер создаётся корректно")
 	public void CourierCreatingTrue_test()
